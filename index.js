be0bbd3e53cc86e3a2dfc97713efb162e74bb471
Kelly <redbeeno.1@gmail.com>
2022-03-30 11:58:31 +0800

    update api name

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/roman/:num?', (req, res) => {
+app.get('/:num?', (req, res) => {
     const roman = intToRoman(req.params.num)
     res.send(roman);
 });
