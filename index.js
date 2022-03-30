b54c3c651fd01ce4383fcba8c2b261870a9cbf5b
Kelly <redbeeno.1@gmail.com>
2022-03-30 11:17:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { intToRoman } from './integerToRoman.js'
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/roman/:num?', (req, res) => {
+    const roman = intToRoman(req.params.num)
+    res.send(roman);
+});
+app.listen(port, () => console.log(`App listening on port ${port}!`))
