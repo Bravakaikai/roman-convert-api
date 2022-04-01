0247821264c85c992c1686d7a8d6f22e4da38efa
Kelly <redbeeno.1@gmail.com>
2022-04-01 16:56:12 +0800

    format code

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import { intToRoman } from './integerToRoman.js'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(cors())
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/:num?', (req, res) => {
-    const roman = intToRoman(req.params.num)
-    res.send(roman)
-});
+  const roman = intToRoman(req.params.num)
+  res.send(roman)
+})
 
 app.post('/roman', (req, res) => {
-    const roman = intToRoman(req.body.num)
-    return res.json({ roman })
-});
+  const roman = intToRoman(req.body.num)
+  return res.json({ roman })
+})
 
 app.post('/romanFromInput', (req, res) => {
-    const { num } = req.body.input
-    const roman = intToRoman(num)
-    return res.json({ roman })
-});
+  const { num } = req.body.input
+  const roman = intToRoman(num)
+  return res.json({ roman })
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
--- integerToRoman.js
+++ integerToRoman.js
@@ -2,76 +2,75 @@
  * @param {number} num
  * @return {string}
  */
-export function  intToRoman (num) {
-    const symbol = [
-        {
-            text: 'M',
-            value: 1000
-        },
-        {
-            text: 'CM',
-            value: 900
-        },
-        {
-            text: 'D',
-            value: 500
-        },
-        {
-            text: 'CD',
-            value: 400
-        },
-        {
-            text: 'C',
-            value: 100
-        },
-        {
-            text: 'XC',
-            value: 90
-        },
-        {
-            text: 'L',
-            value: 50
-        },
-        {
-            text: 'XL',
-            value: 40
-        },
-        {
-            text: 'X',
-            value: 10
-        },
-        {
-            text: 'IX',
-            value: 9
-        },
-        {
-            text: 'V',
-            value: 5
-        },
-        {
-            text: 'IV',
-            value: 4
-        },
-        {
-            text: 'I',
-            value: 1
-        }
-    ]
-    
-    let roman = []
-    if (num >= 1 && num <= 3999) {
-        for (let i = 0; i < symbol.length; i++) {
-            if (num >= symbol[i].value) {
-                let count = Math.floor(num/symbol[i].value)
-                num -= symbol[i].value * count
-                while (count > 0) {
-                    roman.push(symbol[i].text)
-                    count--
-                }
-            }
-        }
+export function intToRoman (num) {
+  const symbol = [
+    {
+      text: 'M',
+      value: 1000
+    },
+    {
+      text: 'CM',
+      value: 900
+    },
+    {
+      text: 'D',
+      value: 500
+    },
+    {
+      text: 'CD',
+      value: 400
+    },
+    {
+      text: 'C',
+      value: 100
+    },
+    {
+      text: 'XC',
+      value: 90
+    },
+    {
+      text: 'L',
+      value: 50
+    },
+    {
+      text: 'XL',
+      value: 40
+    },
+    {
+      text: 'X',
+      value: 10
+    },
+    {
+      text: 'IX',
+      value: 9
+    },
+    {
+      text: 'V',
+      value: 5
+    },
+    {
+      text: 'IV',
+      value: 4
+    },
+    {
+      text: 'I',
+      value: 1
     }
-    
-    return roman.join('') 
-};
+  ]
 
+  let roman = []
+  if (num >= 1 && num <= 3999) {
+    for (let i = 0; i < symbol.length; i++) {
+      if (num >= symbol[i].value) {
+        let count = Math.floor(num / symbol[i].value)
+        num -= symbol[i].value * count
+        while (count > 0) {
+          roman.push(symbol[i].text)
+          count--
+        }
+      }
+    }
+  }
+
+  return roman.join('')
+};
